0b1606d19c8a3ab12d62a2e5d496a1565c103ce0
Tetsu-is <hap30305@kwansei.ac.jp>
2023-11-19 00:53:59 +0900

    make get and post API

--- main.go
+++ main.go
@@ -1,29 +1,55 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-type todo struct {
-	ID        string `json:"id"`
-	Item      string `json:"title"`
-	Completed bool   `json:"completed"`
+type Todo struct {
+	ID   uint   `json:"id"`
+	Task string `json:"task"`
 }
 
-var todos = []todo{
-	{ID: "1", Item: "部屋掃除", Completed: false},
-	{ID: "2", Item: "本を読む", Completed: false},
-	{ID: "3", Item: "買い物", Completed: false},
-}
+var db *gorm.DB
+var err error
 
-// jsonに変換
-func getTodos(context *gin.Context) {
-	context.IndentedJSON(http.StatusOK, todos)
-}
 func main() {
+	//connect to MySQL database
+	dsn := "tetsuro:11quin17@tcp(localhost:3306)/todo?charset=utf8&parseTime=True&loc=Local"
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Failed to connect to MySQL database")
+		return
+	}
+
+	//Automigrate the Todo model
+	db.AutoMigrate(&Todo{})
+
+	//Initialize GIN router
 	router := gin.Default()
+
+	//Define routes
 	router.GET("/todos", getTodos)
+	router.POST("/todos", addTodo)
+
+	//Start the server
 	router.Run("localhost:9090")
+}
+
+// Handler function to get all Todos
+func getTodos(c *gin.Context) {
+	var todos []Todo
+	db.Find(&todos)
+	c.JSON(200, todos)
+}
+
+// Handler function to add a new Todo
+func addTodo(c *gin.Context) {
+	var todo Todo
+	c.BindJSON(&todo)
+	db.Create(&todo)
+	c.JSON(200, todo)
 }
